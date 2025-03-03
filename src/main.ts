2770e49d96dde834d51063bf93a03ea845b2ded0
obaraelijah <elijahobara357@gmail.com>
2025-03-03 15:18:37 +0300

    feat: initialize client sd

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './styles/colors.scss'
+import './styles/typography.scss'
+import './styles/document.scss'
+import './styles/layout.scss'
+import './styles/form.scss'
+import './styles/content.scss'
+
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+
+Alpine.start()
